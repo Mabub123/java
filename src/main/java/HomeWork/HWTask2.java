7bb3cfe880b44f46152cfdf7b7bf02fd99f473ab
Makhbuba <makhbuba18@gmail.com>
2023-05-17 20:15:01 -0400

    homework

---
+++ src/main/java/HomeWork/HWTask2.java
@@ -0,0 +1,16 @@
+package HomeWork;
+
+public class HWTask2 {
+
+    public static void main(String[] args) {
+        int number = 234;
+        int e = number % 10;
+        int d = number / 10 % 10;
+        int s = number / 100;
+
+
+        System.out.println(s);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
